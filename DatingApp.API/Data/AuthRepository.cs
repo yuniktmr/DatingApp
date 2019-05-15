38117e3532b322e06fce09dc1560df0560526fa4
Yunik Tamrakar <yuniktmr@gmail.com>
2019-05-15 15:00:33 -0500

    Authentication module

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,45 @@
+using System.Threading.Tasks;
+using DatingApp.API.Model;
+
+namespace DatingApp.API.Data{
+    public class AuthRepository : IAuthRepository
+    {
+       private readonly DataContext _context;
+       public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> Login(string username, string password)
+        { 
+           
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+             byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt; 
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAync();
+
+            return user;
+           
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt){
+             using (var hmac = new System.Security.Cryptography.HMACSHA512()){
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+              
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
